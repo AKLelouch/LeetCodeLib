c32ecb9c20aac0a85010df732d48f0b7df7ce772
王天琪 <wangtianqi@wangtianqideMacBook-Air.local>
2024-12-09 21:40:49 +0800

    新增74题，新增二分查找算法

---
+++ JavaScript/leetcode/74searchMatrix.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  if (!matrix.length) return false;
+  let m = matrix.length;
+  let n = matrix[0].length;
+  if (target < matrix[0][0] || target > matrix[m - 1][n - 1]) return false;
+
+  const res1 = binarySearchArray(matrix, target);
+  if (res1 === true) return true;
+};
+
+const binarySearchArray = (array, target) => {
+  let i = 0;
+  let j = array.length;
+  while (i < j) {
+    let mid = parseInt((i + j) / 2);
+    if (target < array[mid][0]) {
+      j = mid;
+    } else if (array[mid][0] < target) {
+      i = mid + 1;
+    } else {
+      return true;
+    }
+  }
+  return i - 1;
+};
+const binarySearch = (array, target) => {
+  let i = 0;
+  let j = array.length;
+  while (i < j) {
+    let mid = parseInt((i + j) / 2);
+    if (array[mid] === target) return mid;
+    if (target < array[mid]) {
+      j = mid;
+    }
+    if (array[mid] < target) {
+      i = mid + 1;
+    }
+  }
+  return -1;
+};
---
+++ JavaScript/search/binarySearch.js
@@ -0,0 +1,26 @@
+const binarySearchRecursion = (array, head, tail, target) => {
+  const mid = parseInt((tail + head) / 2);
+  if (array[mid] === target) return mid;
+  if (target < array[mid]) {
+    return binarySearch(array, head, mid, target);
+  }
+  if (array[mid] < target) {
+    return binarySearch(array, mid + 1, tail, target);
+  }
+};
+
+const binarySearchIterate = (array, target) => {
+  let i = 0;
+  let j = array.length;
+  while (i < j) {
+    let mid = parseInt((i + j) / 2);
+    if (array[mid] === target) return mid;
+    if (target < array[mid]) {
+      j = mid;
+    }
+    if (array[mid] < target) {
+      i = mid + 1;
+    }
+  }
+  return -1;
+};
