67b20d7c7ac751c6bac69e2151307e1776552831
AKLelouch <whut_wtq@qq.com>
2024-12-16 01:07:34 +0800

    新增739

---
+++ JavaScript/leetcode/739dailyTemperatures.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+  let singleStack = [];
+  for (const temp of temperatures) {
+  }
+};
+
+class SingleStack {
+  stack = [];
+  constructor() {}
+
+  push(val) {
+    for (const element of this.stack) {
+      
+    }
+  }
+}
