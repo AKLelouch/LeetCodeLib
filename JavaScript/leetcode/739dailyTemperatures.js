a36532707362c79a000a15e0ea5d2e763f0a537b
王天琪 <wangtianqi@wangtianqideMacBook-Air.local>
2024-12-16 15:36:26 +0800

    修改739题

--- JavaScript/leetcode/739dailyTemperatures.js
+++ JavaScript/leetcode/739dailyTemperatures.js
@@ -3,9 +3,12 @@
  * @return {number[]}
  */
 var dailyTemperatures = function (temperatures) {
-  let singleStack = [];
-  for (const temp of temperatures) {
+  let singleStack = new SingleStack();
+  let res = [];
+  for (let i = temperatures.length - 1; i >= 0; i--) {
+    res.unshift(singleStack.push([temperatures[i], i]));
   }
+  return res;
 };
 
 class SingleStack {
@@ -13,8 +16,18 @@ class SingleStack {
   constructor() {}
 
   push(val) {
-    for (const element of this.stack) {
-      
+    const [inputTemp, inputIndex] = val;
+    this.stack.push(val);
+    let min = Number.MAX_SAFE_INTEGER;
+    for (let i = this.stack.length - 2; i >= 0; i--) {
+      const [temp, index] = this.stack[i];
+      if (inputTemp < temp) {
+        this.stack[i + 1] = this.stack[i];
+        this.stack[i] = val;
+        min = Math.min(min, index - inputIndex);
+      }
     }
+    return min === Number.MAX_SAFE_INTEGER ? 0 : min;
   }
 }
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
