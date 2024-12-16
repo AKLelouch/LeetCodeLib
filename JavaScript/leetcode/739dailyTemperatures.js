bc89659ce87f0820fba52fbe23d847e04f56f88c
王天琪 <wangtianqi@wangtianqideMacBook-Air.local>
2024-12-16 19:50:38 +0800

    修改739

--- JavaScript/leetcode/739dailyTemperatures.js
+++ JavaScript/leetcode/739dailyTemperatures.js
@@ -3,11 +3,48 @@
  * @return {number[]}
  */
 var dailyTemperatures = function (temperatures) {
-  let singleStack = new SingleStack();
+  // let singleStack = new SingleStack();
   let res = [];
   for (let i = temperatures.length - 1; i >= 0; i--) {
-    res.unshift(singleStack.push([temperatures[i], i]));
+    // res.unshift(singleStack.push([temperatures[i], i]));
   }
+  let head = new LinkNode(0, 0, null);
+  head.next = new LinkNode(
+    temperatures[temperatures.length - 1],
+    temperatures.length - 1,
+    null
+  );
+  res.unshift(0);
+  for (let i = temperatures.length - 2; i >= 0; i--) {
+    let prevNode = head;
+    let nextNode = head.next;
+    let min = Number.MAX_SAFE_INTEGER;
+    const inputTemp = temperatures[i];
+    const inputIndex = i;
+    while (true) {
+      if (!nextNode) {
+        prevNode.next = new LinkNode(inputTemp, inputIndex, null);
+        break;
+      }
+
+      const temp = nextNode.temperature;
+      const index = nextNode.index;
+      if (inputTemp < temp) {
+        min = Math.min(min, index - inputIndex);
+        prevNode = nextNode;
+        nextNode = nextNode.next;
+      } else if (inputTemp > temp) {
+        const node = new LinkNode(inputTemp, inputIndex, nextNode);
+        prevNode.next = node;
+        break;
+      } else if (inputTemp === temp) {
+        nextNode.index = inputIndex;
+        break;
+      }
+    }
+    res.unshift(min === Number.MAX_SAFE_INTEGER ? 0 : min);
+  }
+
   return res;
 };
 
@@ -25,11 +62,22 @@ class SingleStack {
         this.stack[i + 1] = this.stack[i];
         this.stack[i] = val;
         min = Math.min(min, index - inputIndex);
-      }
-      else if(inputTemp === temp){
+      } else if (inputTemp === temp) {
       }
     }
     return min === Number.MAX_SAFE_INTEGER ? 0 : min;
+  }
+}
+
+class LinkNode {
+  temperature = null;
+  index = null;
+  next = null;
+
+  constructor(temperature, index, next) {
+    this.temperature = temperature;
+    this.index = index;
+    this.next = next;
   }
 }
 console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
