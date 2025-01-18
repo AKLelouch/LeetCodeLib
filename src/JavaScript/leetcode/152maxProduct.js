1214e64b9a59788b8962fe9ba17a75e62d93ec3a
AKLelouch <whut_wtq@qq.com>
2025-01-18 23:38:57 +0800

    更新152题

--- src/JavaScript/leetcode/152maxProduct.js
+++ src/JavaScript/leetcode/152maxProduct.js
@@ -6,17 +6,20 @@ var maxProduct = function (nums) {
   nums.unshift(0);
   let dpMax = new Array(nums.length);
   let dpMin = new Array(nums.length);
-  dpMax[0] = 0;
-  dpMin[0] = 0;
-  for (let i = 1; i < nums.length; i++) {
+  dpMax[1] = nums[1];
+  dpMin[1] = nums[1];
+  let res = nums[1];
+  for (let i = 2; i < nums.length; i++) {
     let current = nums[i];
-    if (nums[i] < 0) {
-      dpMax[i] = Math.max(dpMin[i - 1] * current, dpMax[i - 1]);
-      dpMin[i] = Math.min(dpMax[i - 1] * current, dpMin[i - 1]);
-    } else {
-      dpMax[i] = Math.max();
-    }
+    // if (nums[i] < 0) {
+    dpMax[i] = Math.max(dpMin[i - 1] * current, dpMax[i - 1] * current, current);
+    dpMin[i] = Math.min(dpMin[i - 1] * current, dpMax[i - 1] * current, current);
+    // } else {
+    //   dpMax[i] = Math.max();
+    // }
+    res = Math.max(res, dpMax[i]);
   }
+  return res;
 };
 
 console.log(maxProduct([2, 3, -2, 4]));
