170d39c752574dc44ac25f6459f2a72978ca1a89
AKLelouch <whut_wtq@qq.com>
2025-01-20 20:51:57 +0800

    更新62题

--- src/JavaScript/leetcode/62uniquePaths.js
+++ src/JavaScript/leetcode/62uniquePaths.js
@@ -3,6 +3,15 @@
  * @param {number} n
  * @return {number}
  */
-// var uniquePaths = function (m, n) {
+var uniquePaths = function (m, n) {
+  let dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+  dp[0][1] = 1;
+  for (let i = 1; i < dp.length; i++) {
+    for (let j = 1; j < dp[0].length; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m][n];
+};
 
-// };
+console.log(uniquePaths(3, 7));
