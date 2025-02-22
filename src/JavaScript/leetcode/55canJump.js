920c15ac2439309dbdbc1e898011d9b3d4db872b
AKLelouch <whut_wtq@qq.com>
2025-02-22 23:10:06 +0800

    新增55题，贪心法

---
+++ src/JavaScript/leetcode/55canJump.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+  let max = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > max) return false;
+    if (max >= nums.length - 1) return true;
+    max = Math.max(max, nums[i] + i);
+  }
+  return true;
+};
+
+export { canJump };
